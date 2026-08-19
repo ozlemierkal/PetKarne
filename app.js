7496a07e1217ea51d6d31d18824cec91c1fc45b4
ozlemierkal <ozlemierkal@gmail.com>
2026-08-19 23:51:27 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1248,11 +1248,6 @@ if(selectedCalendarDate===null){
 }
 renderAll();
 
-if('serviceWorker' in navigator){
-  navigator.serviceWorker.getRegistrations().then(regs=>{
-    regs.forEach(r=>r.unregister());
-  }).catch(()=>{});
-}
 if('caches' in window){
   caches.keys().then(keys=>{
     keys.forEach(k=>caches.delete(k));
